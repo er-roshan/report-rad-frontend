1451afbb59934c68afb30ab29fc9df7d046961a2
Roshan Adhikari <roshan.adhikari.se@gmail.com>
2023-02-18 12:57:32 +0545

    Added global classes for form fields and buttons

--- src/components/shared/MainHeader.vue
+++ src/components/shared/MainHeader.vue
@@ -8,7 +8,7 @@
             </div>
             <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
                 <RouterLink :to="props.url"
-                    class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">Add
+                    class="inline-flex items-center justify-center global-btn primary-btn sm:w-auto">Add
                     {{props.title}}</RouterLink>
             </div>
         </div>
