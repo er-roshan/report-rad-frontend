91d0ecadab6ffc31ae5d6934dda79f2a73650951
Roshan Adhikari <roshan.adhikari.se@gmail.com>
2023-02-19 16:39:59 +0545

    Create Report page and forms

---
+++ src/pages/reports/create.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="py-6">
+    <CreateReport />
+  </div>
+
+</template>
+
+<script setup>
+import CreateReport from '../../components/forms/CreateReport.vue';
+
+</script>
--- src/pages/reports/index.vue
+++ src/pages/reports/index.vue
@@ -3,7 +3,7 @@ import MainHeader from '../../components/shared/MainHeader.vue';
 
 <template>
     <div class="py-6">
-        <MainHeader title="Reports" url="/reports" />
+        <MainHeader title="Reports" url="/reports/create" />
         <div class="px-4 sm:px-6 md:px-0">
             <!-- Replace with your content -->
             <div class="py-4">
