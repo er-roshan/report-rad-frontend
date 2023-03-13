5491cfee520aa53bfa84d9506c48189ed4c1da04
Roshan Adhikari <roshan.adhikari.se@gmail.com>
2023-03-13 16:21:32 +0545

    Create: Auth store for global state

---
+++ src/store/auth.js
@@ -0,0 +1,46 @@
+import { defineStore } from 'pinia'
+import { ref, computed } from 'vue'
+import axiosClient from '../axios-client.js'
+
+export const useAuthStore = defineStore('auth', () => {
+
+    const user = ref(null);
+    const token = ref(localStorage.getItem('ACCESS_TOKEN') || null);
+    const loading = ref(false);
+    const loginErrors = ref(null);
+    const registerErrors = ref(null);
+
+    const login = async (payload) => {
+        console.log(payload)
+        loading.value = true;
+        await axiosClient.post('/login', payload).then(({data }) => {
+            loading.value = false;
+            user.value = data.user;
+            localStorage.setItem("ACCESS_TOKEN", data.token);
+        }).catch(err => {
+            const response = err.response;
+            if(response && response.status ===422 ) {
+                loginErrors.value = response.data.errors;
+            }
+        })
+
+    }
+    const register = async (payload) => {
+        console.log(payload)
+        loading.value = true;
+        await axiosClient.post('/register', payload).then(({data }) => {
+            loading.value = false;
+            user.value = data.user;
+            localStorage.setItem("ACCESS_TOKEN", data.token);
+        }).catch(err => {
+            const response = err.response;
+            if(response && response.status ===422 ) {
+                registerErrors.value = response.data.errors;
+            }
+        })
+
+    }
+
+
+    return { user, token,loginErrors,registerErrors, login, register, loading}
+})
